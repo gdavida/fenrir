6ad389c2e483bcb436df35fe144cc4b1b5f815e7
Andrew Ek <andrewek@gmail.com>
2016-03-22 15:30:23 -0500

    Bears and Farms - WIP

---
+++ lesson_plans/week_02/day-02-samples/bear-farm/lib/farm.rb
@@ -0,0 +1,32 @@
+require "pry"
+
+class Farm
+  attr_accessor :acres, :crop, :planted, :harvest_method
+
+  def initialize(acres:, crop:, planted:, harvest_method:)
+    @acres = acres
+    @crop = crop
+    @planted = planted
+    @harvest_method = harvest_method
+  end
+
+  def plant
+    if @planted
+      "Already planted"
+    else
+      @planted = true
+      "Planted successfully"
+    end
+  end
+
+  def harvest
+    if @planted
+      @planted = false
+      "You harvested #{acres} acres of #{crop} by #{harvest_method}"
+    else
+      "You cannot harvest an unplanted field"
+    end
+  end
+end
+
+
---
+++ lesson_plans/week_02/day-02-samples/bear-farm/tests/farm_test.rb
@@ -0,0 +1,55 @@
+require_relative "test_helper"
+require_relative "../lib/farm.rb"
+
+class FarmTest < Minitest::Test
+
+  def test_should_be_created_under_normal_cirumstances
+    farm = Farm.new(acres: 22, crop: "bananas", planted: true, harvest_method: :hand)
+    refute_nil(farm)
+  end
+
+  def test_acres_getter
+    farm = Farm.new(acres: 22, crop: "bananas", planted: true, harvest_method: :hand)
+    assert_equal(22, farm.acres)
+  end
+
+  def test_farm_should_be_plantable
+    farm = Farm.new(acres: 22, crop: "bananas", planted: true, harvest_method: :hand)
+    assert_respond_to(farm, :plant)
+  end
+
+  def test_planting_an_unplanted_farm
+    farm = Farm.new(acres: 22, crop: "bananas", planted: false, harvest_method: :hand)
+    result = farm.plant
+
+    assert(farm.planted == true, "Farm should be planted")
+
+    assert_equal("Planted successfully", result)
+  end
+
+  def test_planting_an_already_planted_farm
+    farm = Farm.new(acres: 22, crop: "bananas", planted: true, harvest_method: :hand)
+
+    result = farm.plant
+
+    assert_equal("Already planted", result)
+  end
+
+  def test_harvesting_a_planted_field
+    farm = Farm.new(acres: 22, crop: "bananas", planted: true, harvest_method: :hand)
+
+    result = farm.harvest
+
+    refute(farm.planted, "After harvesting, the farm should be unplanted")
+    assert_equal("You harvested 22 acres of bananas by hand", result)
+  end
+
+  def test_harvesting_an_unplanted_field
+    farm = Farm.new(acres: 22, crop: "bananas", planted: false, harvest_method: :hand)
+
+    result = farm.harvest
+
+    refute(farm.planted, "Should still be unplanted")
+    assert_equal("You cannot harvest an unplanted field", result)
+  end
+end
