d7a24d4a78bbfb8a8ed835b0ca12a429bc960b55
Andrew Ek <andrewek@gmail.com>
2016-04-25 16:15:56 -0500

    Code samples

--- lesson_plans/week_07/day_01_samples/script.js
+++ lesson_plans/week_07/day_01_samples/script.js
@@ -1,9 +1,6 @@
 console.log("Hello!");
 console.log("This is the log");
 
-var sum = 5 + 7;
-console.log(sum);
-
-for (var i = 10; i >= 1; i-- ) {
-  console.log(i);
+function sayHi() {
+  alert("Hellooooooooooo");
 }
