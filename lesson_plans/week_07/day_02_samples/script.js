d7d64c31254f422417ab85d3277c86ae81230741
Andrew Ek <andrewek@gmail.com>
2016-04-26 14:36:28 -0500

    Updated samples

--- lesson_plans/week_07/day_02_samples/script.js
+++ lesson_plans/week_07/day_02_samples/script.js
@@ -1,1 +1,23 @@
 // Put some scripts here?
+
+function makeRed() {
+  var headers = document.getElementsByClassName("header");
+  for (var i = 0; i < headers.length; i++) {
+    addClass(headers[i], "red");
+  }
+}
+
+function returnToBlack() {
+  var headers = document.getElementsByClassName("header");
+  for (var i = 0; i < headers.length; i++) {
+    headers[i].classList.remove("red");
+  }
+}
+
+function makePurple(el) {
+  addClass(el, "purple");
+}
+
+function addClass(element, className) {
+  element.classList.add(className);
+}
